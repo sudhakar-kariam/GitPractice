e1483ba75ec3d8b7810c20c86147e88d33c1e7cc
Sudhakar k <sudhakar.kariam@logictreeit.in>
2023-04-14 16:09:22 +0530

    New Release commit

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var prod: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
